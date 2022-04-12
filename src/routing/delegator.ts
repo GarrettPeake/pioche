9280bd4904602be0724b19c90ac02a496c93b83d
Garrett Peake <gepeake@gmail.com>
2022-04-11 22:15:37 -0700

    Add support for scheduled events

--- src/routing/delegator.ts
+++ src/routing/delegator.ts
@@ -1,27 +1,33 @@
 import { Session } from '../io/input';
-import { Logger } from '../logging/logger';
 import { Router } from './router';
-// TODO: use config to export all of the controllers
 
-export default{
-    /**
-     * Entry point for the worker using modules worker syntax
-     * @param request Holds information about the HTTP request
-     * @param env Holds information about the worker's environment
-     * @returns A promise containing a response
-     */
-    async fetch(request: Request, env: any) {
-        let session = new Session(request)
-        session.logger.log('REQUEST RECEIVED');
+/**
+ * Entry point for the worker using modules worker syntax
+ * @param request Holds information about the HTTP request
+ * @param env Holds information about the worker's environment
+ * @returns A promise containing a response
+ */
+export async function handleFetch(request: Request, env: any) {
+    let session = new Session(request)
+    session.logger.log('REQUEST RECEIVED');
 
-        // Make the environment available to the entire framework
-        globalThis.env = env;
+    // Make the environment available to the entire framework
+    globalThis.env = env;
 
-        // TODO: Run a user provided configuration script
-        
-        // TODO: Remember to handle preflights 'easyPreflight'
-        
-        // Route request
-        return Router.route(session)
-    }
+    // TODO: Run a user provided configuration script
+    
+    // TODO: Remember to handle preflights 'easyPreflight'
+    
+    // Route request
+    return Router.route(session)
+}
+
+export async function handleScheduled(event: any, env: any) {
+    handleFetch(
+        // TODO: ADD INTERNAL PERMS TO REQUEST
+        new Request(
+            "https://www.dummy-url.com/cron?trigger=" + event.cron
+        ),
+        env
+    )
 }
